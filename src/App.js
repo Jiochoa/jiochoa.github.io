77b01684473687a4fbc9786dda91bc3b7dfbd955
jiochoa <jochoa105@gmail.com>
2019-06-25 23:09:19 -0600

    using React

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import TableOfContent from './components/TableOfContent/TableOfContent';
+import Content from './components/Content/Content';
+import Footer from './components/Footer/Footer';
+
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <TableOfContent />
+      <Content />
+      <Footer />
+      {
+        
+      }
+    </div>
+  );
+}
+
+export default App;
