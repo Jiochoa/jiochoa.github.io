af39d33fd85e0e2361f7dc2c82758d2f376e9b6b
jiochoa <jochoa105@gmail.com>
2020-08-25 07:43:41 -0600

    added modularity

---
+++ precache-manifest.ba53dc1a9d0ae76af77af088c4b6c8aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bf7b17a00d2b1a71c314e5df67f5251",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f5ad2048c729b45d2cb",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "6d49bf6e9d537af020ba",
+    "url": "/static/css/main.80f4a809.chunk.css"
+  },
+  {
+    "revision": "6f5ad2048c729b45d2cb",
+    "url": "/static/js/2.5d03fa34.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5d03fa34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d49bf6e9d537af020ba",
+    "url": "/static/js/main.6bb70a4d.chunk.js"
+  },
+  {
+    "revision": "506adfa26ad76816c4a2",
+    "url": "/static/js/runtime-main.c34c0ec6.js"
+  }
+]);
