c9677777e547d3d0da92a457361c4fa3162931c4
Juan Ochoa <jochoa105@gmail.com>
2023-06-04 09:36:45 -0600

    Update Postamble.js

--- react_build/personal_website/src/components/Postamble/Postamble.js
+++ react_build/personal_website/src/components/Postamble/Postamble.js
@@ -5,7 +5,7 @@ const Postamble = () => {
     return (
         <div id="postamble" class="status">
             <p class="author">Author: Juan Ochoa</p>
-            <p class="date">Updated: 8/25/2020 12:57 PM</p>
+            <p class="date">Updated: 6/04/2023 12:57 PM</p>
             <p class="validation"><a href="theWebsite">Validate</a></p>
         </div>
     );
